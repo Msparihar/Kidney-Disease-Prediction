bcddb528247873c37672b71ca6edd2eb14a756ef
Msparihar <manishsparihar2020@gmail.com>
2023-10-27 23:54:09 +0530

    Update setup and readme

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "krishnaik06"
+REPO_NAME = "KIDNEY-DISEASE-PREDICTION"
+AUTHOR_USER_NAME = "Msparihar"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "manishsparihar2020@gmail.com"
 
 
 setuptools.setup(
