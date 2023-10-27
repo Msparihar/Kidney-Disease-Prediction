e596518448e0e4b7effcfe779900d827a0e650bb
Msparihar <manishsparihar2020@gmail.com>
2023-10-28 00:11:39 +0530

    First test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import os
 from flask_cors import CORS, cross_origin
-from cnnClassifier.utils.common import decodeImage
+from src.cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.prediction import PredictionPipeline
 
 
